79c77bc47ec144ea1cf41efa53de12d264503fe2
Marina Fando <marinafando@gmail.com>
2023-04-28 04:43:58 +0200

    feat: add modules dependencies

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CartComponent } from './components/cart/cart.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
+import { CartRoutingModule } from './cart-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-  ],
+  declarations: [CartComponent, CartPageComponent],
+  imports: [CommonModule, CartRoutingModule, SharedModule],
   exports: [],
 })
-export class CartModule { }
+export class CartModule {}
