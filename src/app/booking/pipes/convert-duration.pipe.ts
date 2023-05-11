c346d424cf3b7156a3dd6824272f1afb30b056c4
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-05-11 16:18:25 +0300

    fix: fix flight-selection pipes

--- src/app/booking/pipes/convert-duration.pipe.ts
+++ src/app/booking/pipes/convert-duration.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertDurationPipe implements PipeTransform {
   transform(durationMin: number): string {
-    const hours = Math.round(durationMin / 60);
+    const hours = Math.floor(durationMin / 60);
     const minutes = durationMin % 60;
     // eslint-disable-next-line no-nested-ternary
     return hours ? (minutes ? `${hours} h ${minutes} min` : `${hours} h`) : `${minutes} min`;
