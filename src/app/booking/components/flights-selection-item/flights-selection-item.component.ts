76b05ff21dda7936edf892703f54f44313c57509
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-05-13 15:29:14 +0300

    feat: add basic carousel representation for flights selection

--- src/app/booking/components/flights-selection-item/flights-selection-item.component.ts
+++ src/app/booking/components/flights-selection-item/flights-selection-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { IAirResponse } from 'src/app/shared/models/interfaces';
+import { IAirResponse, IFlightInfo } from 'src/app/shared/models/interfaces';
 
 @Component({
   selector: 'app-flights-selection-item',
@@ -12,4 +12,106 @@ export class FlightsSelectionItemComponent {
   @Input() response: IAirResponse;
 
   @Input() source: string;
+
+  slides: IFlightInfo[];
+
+  slideConfig = {
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    initialSlide: 2,
+    infinite: false,
+    draggable: false,
+    arrows: true,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  slideConfigBack: any;
+
+  // addSlide() {
+  //   this.slides.push({ img: 'http://placehold.it/350x150/777777' });
+  // }
+
+  // removeSlide() {
+  //   this.slides.length -= 1;
+  // }
+
+  slickInit(event: any) {
+    console.log('slick initialized', event);
+  }
+
+  breakpoint(event: any) {
+    console.log('breakpoint', event);
+  }
+
+  afterChange(event: any) {
+    console.log('afterChange', event);
+  }
+
+  beforeChange(event: any) {
+    console.log('beforeChange', event);
+  }
+
+  ngOnInit() {
+    this.slides = this.response.thereWay;
+
+    this.slideConfigBack = {
+      slidesToShow: 5,
+      slidesToScroll: 2,
+      initialSlide: this.slides.length - 3,
+      infinite: false,
+      draggable: false,
+      arrows: true,
+      dots: false,
+      centerMode: true,
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    }
+  }
 }
---
+++ src/app/booking/pipes/sequence-date.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SecuenceDatePipe } from './sequence-date.pipe';
+
+describe('SecuenceDatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new SecuenceDatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
