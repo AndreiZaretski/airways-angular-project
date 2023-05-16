ea07ddaa222cfed9a7b4a47347594422a0137c30
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-05-16 16:41:20 +0300

    feat: add select/edit functionality

--- src/app/booking/components/flights-selection-item/flights-selection-item.component.ts
+++ src/app/booking/components/flights-selection-item/flights-selection-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { updateChooseChekedBackWay, updateChooseChekedBackWayEdit, updateChooseChekedThereWay, updateChooseChekedThereWayEdit, updateIndexBackWay, updateIndexThereWay } from 'src/app/redux/actions/state.actions';
+import { selectUserBooking } from 'src/app/redux/selectors/state.selector';
 import { IAirResponse } from 'src/app/shared/models/interfaces';
 
 @Component({
@@ -12,6 +15,10 @@ export class FlightsSelectionItemComponent implements OnInit {
   @Input() response: IAirResponse;
 
   @Input() source: string;
+
+  checkedThereWay = false;
+
+  checkedBackWay = false;
 
   flightCardConfig = {
     slidesToShow: 1,
@@ -105,6 +112,10 @@ export class FlightsSelectionItemComponent implements OnInit {
     ],
   };
 
+  userBooking$ = this.store.select(selectUserBooking);
+
+  constructor(private store: Store) {}
+
   slickInit(event: any) {
     console.log('slick initialized', event);
   }
@@ -125,6 +136,31 @@ export class FlightsSelectionItemComponent implements OnInit {
     if (this.response.backWay) {
       this.slideConfigBack.initialSlide = this.response.backWay.length - 4;
       this.flightCardConfigBack.initialSlide = this.response.backWay.length - 4;
+    }
+
+    this.userBooking$.subscribe((res) => {
+      this.checkedThereWay = res.checkedThereWay;
+      this.checkedBackWay = res.checkedBackWay;
+    });
+  }
+
+  editSelection(i: number) {
+    console.log('flight to edit', i);
+    if (this.source !== 'backway') {
+      this.store.dispatch(updateChooseChekedThereWayEdit());
+    } else {
+      this.store.dispatch(updateChooseChekedBackWayEdit());
+    }
+  }
+
+  selectFlight(i: number) {
+    console.log('flight selected', i);
+    if (this.source !== 'backway') {
+      this.store.dispatch(updateChooseChekedThereWay());
+      this.store.dispatch(updateIndexThereWay({ newIndexThereWay: i }));
+    } else {
+      this.store.dispatch(updateChooseChekedBackWay());
+      this.store.dispatch(updateIndexBackWay({ newIndexBackWay: i }));
     }
   }
 }
