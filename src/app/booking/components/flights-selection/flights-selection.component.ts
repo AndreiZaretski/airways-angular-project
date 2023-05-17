759f757faf6e749c58ce792f1474eb88f3a9ed31
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-05-17 17:46:32 +0300

    feat: add continue button logic for flight selection

--- src/app/booking/components/flights-selection/flights-selection.component.ts
+++ src/app/booking/components/flights-selection/flights-selection.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { selectAirResponse } from 'src/app/redux/selectors/state.selector';
+import { selectAirResponse, selectUserBooking } from 'src/app/redux/selectors/state.selector';
 import { EditPanelService } from 'src/app/shared/services/edit-panel.service';
 
 @Component({
@@ -9,9 +9,15 @@ import { EditPanelService } from 'src/app/shared/services/edit-panel.service';
   styleUrls: ['./flights-selection.component.scss'],
 })
 export class FlightsSelectionComponent implements OnInit {
+  checkedThereWay = false;
+
+  checkedBackWay = false;
+
   index = 0;
 
   responseDetails$ = this.store.select(selectAirResponse);
+
+  userBooking$ = this.store.select(selectUserBooking);
 
   constructor(private store: Store, public editPanelService: EditPanelService) {}
 
@@ -19,5 +25,10 @@ export class FlightsSelectionComponent implements OnInit {
     console.log('response', this.responseDetails$);
 
     this.responseDetails$.subscribe((res) => console.log(res));
+
+    this.userBooking$.subscribe((res) => {
+      this.checkedThereWay = res.checkedThereWay;
+      this.checkedBackWay = res.checkedBackWay;
+    });
   }
 }
