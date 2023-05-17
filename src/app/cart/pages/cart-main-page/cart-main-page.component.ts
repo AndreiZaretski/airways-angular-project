7db7392e9de1aba570fde15bd0bc62a9f680ed2f
Marina Fando <marinafando@gmail.com>
2023-05-17 13:51:43 +0200

    feat: add cart-main-page component

---
+++ src/app/cart/pages/cart-main-page/cart-main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-main-page',
+  templateUrl: './cart-main-page.component.html',
+  styleUrls: ['./cart-main-page.component.scss']
+})
+export class CartMainPageComponent {
+
+}
