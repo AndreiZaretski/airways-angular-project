72b3e11fa1b387b61a514ee428cbace7ed3b3cf1
Marina Fando <marinafando@gmail.com>
2023-05-26 04:38:13 +0200

    feat: add service to calculate flight sum

--- src/app/booking/components/total-sum/total-sum.component.ts
+++ src/app/booking/components/total-sum/total-sum.component.ts
@@ -1,52 +1,81 @@
-import { Component, DoCheck, Input } from '@angular/core';
-import { ICurrentFlightSummary } from '../../pages/summary-page/summary-page.component';
+import {
+  Component,
+  DoCheck,
+  Input,
+  OnInit,
+} from '@angular/core';
 import {
   IPassengersCount,
-  IPassengersData,
 } from 'src/app/shared/models/interface-user-booking';
+import { ResultFlightSumService } from 'src/app/core/services/result-flight-sum.service';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { selectUserSettings } from 'src/app/redux/selectors/state.selector';
+import { ICurrentFlightSummary } from '../../pages/summary-page/summary-page.component';
 
 @Component({
   selector: 'app-total-sum',
   templateUrl: './total-sum.component.html',
   styleUrls: ['./total-sum.component.scss'],
 })
-export class TotalSumComponent implements DoCheck {
+export class TotalSumComponent implements OnInit, DoCheck {
   @Input() currentFlight: ICurrentFlightSummary[] = [];
+
   @Input() passengersCountWithTypes: IPassengersCount = {
     adult: 0,
     child: 0,
     infant: 0,
   };
 
-  fareThere: number = 0;
+  fareThere: number;
 
-  fareBack: number = 0;
+  fareBack: number;
 
   typesOfPassengers = Object.keys(this.passengersCountWithTypes);
 
-  ngDoCheck() {
-    if (this.currentFlight[0]?.flightData?.price?.usd) {
-      this.fareThere = this.currentFlight[0]?.flightData?.price?.usd;
-    }
-    if (this.currentFlight[1]?.flightData?.price?.usd) {
-      this.fareBack = this.currentFlight[1]?.flightData?.price?.usd;
-    }
+  userCurrency: string;
+
+  userSettings$ = this.store.select(selectUserSettings);
+
+  private subscriptionUserSettings: Subscription;
+
+  constructor(
+    private resultFlightSumService: ResultFlightSumService,
+    private store: Store,
+  ) {}
+
+  ngOnInit() {
+    this.subscriptionUserSettings = this.userSettings$.subscribe(
+      (res) => (this.userCurrency = res.currency),
+    );
   }
 
-  calculateFare(type: string) {
-    const totalFare = this.fareThere + this.fareBack;
-    return type === 'adult'
-      ? totalFare
-      : type === 'child'
-      ? totalFare * 0.8
-      : totalFare * 0.3;
+  ngDoCheck() {
+    if (this.currentFlight[0]?.flightData?.price) {
+      this.fareThere = this.currentFlight[0]?.flightData?.price[this.userCurrency.toLowerCase()];
+    }
+    if (this.currentFlight[1]?.flightData?.price) {
+      this.fareBack = this.currentFlight[1]?.flightData?.price[this.userCurrency.toLowerCase()];
+    }
   }
 
   calculateTotal() {
     return Object.keys(this.passengersCountWithTypes).reduce(
-      (acc, rec) =>
-        acc + this.passengersCountWithTypes[rec] * this.calculateFare(rec),
-      0
+      (acc, rec) => acc + this.passengersCountWithTypes[rec] * this.resultFlightSumService
+        .calculateFareByType(
+          rec,
+          this.fareThere,
+          this.fareBack,
+        ),
+      0,
+    );
+  }
+
+  calculateFare(type: string) {
+    return this.resultFlightSumService.calculateFareByType(
+      type,
+      this.fareThere,
+      this.fareBack,
     );
   }
 }
--- src/app/cart/components/cart/cart.component.ts
+++ src/app/cart/components/cart/cart.component.ts
@@ -1,29 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Path } from 'src/app/shared/enums/router.enum';
-import { PeriodicElement } from '../table/table.component';
 import { IBookingPage } from 'src/app/shared/models/interface-user-booking';
 import { Store } from '@ngrx/store';
 import { addToFlightsHistory, deleteOrderCart } from 'src/app/redux/actions/state.actions';
+import { ResultFlightSumService } from 'src/app/core/services/result-flight-sum.service';
+import { selectUserSettings } from 'src/app/redux/selectors/state.selector';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
   selectedFlights: IBookingPage[] = [];
 
   path = Path;
 
-  constructor(private store: Store) {}
+  userCurrency: string;
+
+  userSettings$ = this.store.select(selectUserSettings);
+
+  private subscriptionUserSettings: Subscription;
+
+  constructor(
+    private store: Store,
+    private resultFlightSumService: ResultFlightSumService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.subscriptionUserSettings = this.userSettings$.subscribe(
+      (res) => (this.userCurrency = res.currency),
+    );
+  }
 
   getTotalCost() {
     return this.selectedFlights
-      .map((flight) =>
-        flight.responseAir
-          ? flight.responseAir.thereWay[flight.indexThereWay].price.usd
-          : 0
-      )
+      .map((flight) => this.resultFlightSumService
+        .calculateTotalSum(flight, this.userCurrency.toLowerCase()))
       .reduce((acc, value) => acc + value, 0);
   }
 
@@ -32,11 +48,14 @@ export class CartComponent {
   }
 
   payForOrder() {
-    if(this.selectedFlights.length) {
-      this.store.dispatch(addToFlightsHistory({ newBoughtFlights: this.selectedFlights}));
-      this.selectedFlights.map((flight) =>
-        flight.orderId && this.store.dispatch(deleteOrderCart({ OrderId: flight.orderId }))
+    if (this.selectedFlights.length) {
+      this.store.dispatch(
+        addToFlightsHistory({ newBoughtFlights: this.selectedFlights }),
       );
+      this.selectedFlights
+        .map((flight) => flight.orderId && this.store
+          .dispatch(deleteOrderCart({ OrderId: flight.orderId })));
     }
   }
+
 }
