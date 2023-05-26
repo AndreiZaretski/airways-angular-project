6d0af8d89ac9f44d10e0a89be3d40f6ebd50021e
Marina Fando <marinafando@gmail.com>
2023-05-26 04:51:36 +0200

    feat: show table data depending on its type

--- src/app/cart/components/table/table.component.ts
+++ src/app/cart/components/table/table.component.ts
@@ -1,17 +1,32 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { SelectionModel } from '@angular/cdk/collections';
 import {
-  AfterViewInit, Component, DoCheck, EventEmitter, OnInit, Output, ViewChild,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
 } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
-import { Store, select } from '@ngrx/store';
-import { BehaviorSubject, ObjectUnsubscribedError, Observable } from 'rxjs';
-import { checkCart, deleteOrderCart } from 'src/app/redux/actions/state.actions';
-import { selectOrderId } from 'src/app/redux/selectors/state.selector';
-import { selectCartPage } from 'src/app/redux/selectors/state.selector';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { ResultFlightSumService } from 'src/app/core/services/result-flight-sum.service';
+import { StepperService } from 'src/app/core/services/stepper-service.service';
+import {
+  checkRequestUser,
+  deleteOrderCart,
+  replaceOrderCart,
+  watchDetailsOrder,
+} from 'src/app/redux/actions/state.actions';
+import {
+  selectUserSettings,
+  selectCartPage,
+  selectCartPageHistory,
+} from 'src/app/redux/selectors/state.selector';
 import { Path } from 'src/app/shared/enums/router.enum';
 import { IBookingPage } from 'src/app/shared/models/interface-user-booking';
 
@@ -60,30 +75,47 @@ export class TableComponent implements OnInit, DoCheck, AfterViewInit {
 
   path = Path;
 
+  userCurrency: string;
+
+  userSettings$ = this.store.select(selectUserSettings);
+
   @ViewChild(MatSort) sort: MatSort = new MatSort();
 
   cart$: IBookingPage[] = [];
 
-  dataSource = new MatTableDataSource(this.cart$);
+  dataSource = new MatTableDataSource<IBookingPage>([]);
 
+  pageHistory$: IBookingPage[] = [];
+
+  private subscriptionUserSettings: Subscription;
 
   constructor(
+    public stepper: StepperService,
     private liveAnnouncer: LiveAnnouncer,
     private router: Router,
     private store: Store,
+    private resultFlightSumService: ResultFlightSumService,
   ) {
-    this.store.pipe(select(selectCartPage)).subscribe(cart => {
-      this.cart$ = cart
+    this.store.select(selectCartPage).subscribe((cart) => {
+      this.cart$ = cart;
       this.selection.clear();
-    })
+    });
+    this.store.select(selectCartPageHistory).subscribe((pageHistory) => {
+      this.pageHistory$ = pageHistory;
+    });
   }
 
   ngOnInit(): void {
+    this.store.dispatch(checkRequestUser());
 
     this.isVisibleInCart = this.isVisibleColumnInCart();
     this.flightsColumns = this.isVisibleInCart
-    ? this.flightsInCart
-    : this.flightsHistory;
+      ? this.flightsInCart
+      : this.flightsHistory;
+
+    this.subscriptionUserSettings = this.userSettings$.subscribe(
+      (res) => (this.userCurrency = res.currency),
+    );
   }
 
   isVisibleColumnInCart() {
@@ -91,9 +123,10 @@ export class TableComponent implements OnInit, DoCheck, AfterViewInit {
   }
 
   ngDoCheck(): void {
-    this.dataSource = new MatTableDataSource(this.cart$);
+    this.dataSource = new MatTableDataSource(
+      this.isVisibleInCart ? this.cart$ : this.pageHistory$,
+    );
     this.selectedFlights.emit(this.selection.selected);
-
   }
 
   ngAfterViewInit() {
@@ -122,11 +155,22 @@ export class TableComponent implements OnInit, DoCheck, AfterViewInit {
     }
   }
 
-  editFlightDetails() {
-    this.router.navigateByUrl(`${Path.Booking}`);
+  editFlightDetails(id: string) {
+    this.store.dispatch(replaceOrderCart({ OrderId: id }));
+    this.router.navigateByUrl(`${Path.Booking}/${Path.Flights}`);
   }
 
   deleteFlight(id: string): void {
     this.store.dispatch(deleteOrderCart({ OrderId: id }));
+  }
+
+  showFlightDetails(id: string) {
+    // this.stepper.lastStep();
+    this.store.dispatch(watchDetailsOrder({ OrderId: id }));
+    this.router.navigateByUrl(`/${Path.Booking}/${Path.Summary}`);
+  }
+
+  getTotalSum(flight: IBookingPage, userCurrency: string) {
+    return this.resultFlightSumService.calculateTotalSum(flight, userCurrency);
   }
 }
