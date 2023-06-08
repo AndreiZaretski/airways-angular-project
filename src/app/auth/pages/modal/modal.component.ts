fc8f8c289449277a7083db09512c3196bd63e899
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-06-08 23:22:08 +0300

    fix: fix dropdown bug

--- src/app/auth/pages/modal/modal.component.ts
+++ src/app/auth/pages/modal/modal.component.ts
@@ -2,7 +2,6 @@ import {
   Component, OnDestroy, OnInit,
 } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-// import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-modal',
@@ -12,47 +11,20 @@ import { MatDialogRef } from '@angular/material/dialog';
 export class ModalComponent implements OnInit, OnDestroy {
   selectedTab = 0;
 
-  // private backdropClickSub: Subscription;
-
-  // private afterOpenedSub: Subscription;
-
-  // private afterClosedSub: Subscription;
-
   constructor(private dialogRef: MatDialogRef<ModalComponent>) { }
-  // private cd: ChangeDetectorRef  )
 
   ngOnDestroy(): void {
-    // setTimeout(() => {
-    // this.backdropClickSub?.unsubscribe();
-    // this.afterOpenedSub?.unsubscribe();
-    // this.afterClosedSub?.unsubscribe();
-    // });
     this.setOverflowToVisible();
-    // this.cd.detectChanges();
-    // console.log('component destroy');
   }
 
   ngOnInit(): void {
-    // this.backdropClickSub = this.dialogRef.backdropClick().subscribe(() => {
-    //   this.setPointerEventsToNone();
-    // });
     this.setOverflowToAuto();
-
-    // this.afterOpenedSub = this.dialogRef.afterOpened().subscribe(() => {
-    //   this.setOverflowToAuto();
-    // });
-
-    // this.afterClosedSub = this.dialogRef.afterClosed().subscribe(() => {
-    //   this.setPointerEventsToNone();
-    // });
   }
 
   private setOverflowToVisible() {
     const overlayContainer = document.querySelector('.cdk-overlay-container');
 
     if (overlayContainer instanceof HTMLElement) {
-      // overlayContainer.style.pointerEvents = 'none';
-      overlayContainer.style.position = 'absolute';
       overlayContainer.style.overflow = 'visible';
     }
   }
@@ -60,8 +32,6 @@ export class ModalComponent implements OnInit, OnDestroy {
   private setOverflowToAuto() {
     const overlayContainer = document.querySelector('.cdk-overlay-container');
     if (overlayContainer instanceof HTMLElement) {
-      // overlayContainer.style.pointerEvents = 'auto';
-      overlayContainer.style.position = 'fixed';
       overlayContainer.style.overflow = 'auto';
     }
   }
